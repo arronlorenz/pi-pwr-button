22831507f8cd375bf9fdbdf33967611c8799c948
Arron Lorenz <arronlorenz@gmail.com>
2025-06-18 23:49:03 -0500

    Improve soft shutdown script

--- x708-softsd.sh
+++ x708-softsd.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
 BUTTON=13
 
-echo "$BUTTON" > /sys/class/gpio/export;
+echo "$BUTTON" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BUTTON/direction
 echo "1" > /sys/class/gpio/gpio$BUTTON/value
+
+cleanup() {
+  echo "$BUTTON" > /sys/class/gpio/unexport
+}
+
+trap cleanup EXIT
 
 SLEEP=${1:-4}
 
@@ -13,8 +20,8 @@ if ! [[ $SLEEP =~ $re ]] ; then
    echo "error: sleep time not a number" >&2; exit 1
 fi
 
-echo "Your device will shutting down in 4 seconds..."
-/bin/sleep $SLEEP
+echo "Your device will shut down in $SLEEP seconds..."
+/bin/sleep "$SLEEP"
 
 # restore GPIO 13
 echo "0" > /sys/class/gpio/gpio$BUTTON/value
